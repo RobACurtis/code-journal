06f1b6edb2e8dacbe337e1d0098f52ae278e624d
Rob Curtis <Robanthonycurtis@gmail.com>
2022-04-04 23:36:24 +0000

    add a div, add a form, form looks nice, entry required, listen for input events to update the src attribute of the photo, and listen for submit events, put input values into a new object

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
 /* global data */
 /* exported data */
+var $photoURL = document.querySelector('#photoURL');
+$photoURL.addEventListener('input', updateURL);
+
+var $form = document.querySelector('form');
+$form.addEventListener('submit', submit);
+
+function updateURL(event) {
+  var src = $photoURL.value;
+  $photoURL.setAttribute('src', src);
+}
+
+function submit(event) {
+  event.preventDefault();
+  var inputObj = {
+    title: $form.elements.title.value,
+    imageURL: $form.elements.photoURL.value,
+    notes: $form.elements.notes.value
+  };
+
+  var inputString = JSON.stringify(inputObj);
+  localStorage.setItem('Input Form', inputString);
+
+}
