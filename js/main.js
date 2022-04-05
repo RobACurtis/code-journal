13e2e12cd9186784ef020e58ae999fb5ce4f1b8d
Rob Curtis <Robanthonycurtis@gmail.com>
2022-04-05 21:52:57 +0000

    completed DOM tree, for loop, removed example entries

--- js/main.js
+++ js/main.js
@@ -8,6 +8,9 @@ var $photo = document.querySelector('img');
 var $photoURL = document.querySelector('#photoURL');
 $photoURL.addEventListener('input', updateURL);
 
+var $entries = document.querySelector('div[data-view=entries]');
+var $ul = document.querySelector('.entry-list');
+
 function updateURL(event) {
   var src = $photoURL.value;
   $photo.setAttribute('src', src);
@@ -21,8 +24,60 @@ function submitButton(event) {
     notes: $form.elements.notes.value,
     entryId: data.nextEntryId
   };
-  data.nextEntryId += 1;
   data.entries.unshift(inputObj);
+  data.nextEntryId += 1;
   $photo.setAttribute('src', '../images/placeholder-image-square.jpg');
   $form.reset();
+}
+
+function renderEntries(obj) {
+  // ul
+  // --li
+  // ---div
+  // ----img
+  // ---div
+  // ---div
+  // ----h2
+  // ----p
+  // ---div
+  // --li
+  // ul
+
+  $form.setAttribute('class', 'hidden');
+  $entries.setAttribute('class', 'container');
+
+  var _li = document.createElement('li');
+  _li.setAttribute('class', 'row margin-top');
+  $ul.appendChild(_li);
+
+  var _divColumn = document.createElement('div');
+  _divColumn.setAttribute('class', 'column-half');
+  _li.appendChild(_divColumn);
+
+  var _image = document.createElement('img');
+  _image.setAttribute('src', obj.imageURL);
+  _image.className = 'img-size';
+  _divColumn.appendChild(_image);
+
+  var _divText = document.createElement('div');
+  _divText.className = 'column-half margin-bottom';
+  _li.appendChild(_divText);
+
+  var _heading = document.createElement('h2');
+  _heading.className = 'margin black';
+  _heading.textContent = obj.title;
+  _divText.appendChild(_heading);
+
+  var _text = document.createElement('p');
+  _text.className = 'font-weight-four';
+  _text.textContent = obj.notes;
+  _divText.appendChild(_text);
+}
+
+var dataIndex = 0;
+window.addEventListener('DOMContentLoaded', renderExisitingEntries);
+function renderExisitingEntries(event) {
+  for (dataIndex = 0; dataIndex < data.entries.length; dataIndex++) {
+    renderEntries(data.entries[dataIndex]);
+  }
 }
